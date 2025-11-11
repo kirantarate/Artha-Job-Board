c4271aadc8c3f064386c812d963f6a2a93e70d3f
kiran <kirantarate800@gmail.com>
2025-11-11 09:57:12 +0530

    changes next config

--- client/next.config.js
+++ client/next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   // Disable Turbopack (use stable Webpack instead)
   // Turbopack in Next.js 16 has known stability issues
-  experimental: {
-    turbo: false,
-  },
+  // Note: `experimental.turbo` is unrecognized in this Next.js version and
+  // causes warnings/errors. If you need to opt out of Turbopack, do so by
+  // using the supported flags or running with the webpack dev server.
+  // experimental: { turbo: false }, // removed because it's not supported
 }
 
 module.exports = nextConfig
